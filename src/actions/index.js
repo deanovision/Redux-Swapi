4c655789376143f35693d6a95f98df604717dedd
deanovision <rodeanfraser@gmail.com>
2019-04-10 16:18:02 -0400

    finished mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,28 @@
+import axios from "axios";
+
+export const GET_CHARACTERS_FETCHING = "GET_CHARACTERS_FETCHING";
+export const GET_CHARACTERS_SUCCESS = "GET_CHARACTERS_SUCCESS";
+export const GET_CHARACTERS_FAILURE = "GET_CHARACTERS_FAILURE";
+
+export const getCharacters = () => dispatch => {
+  dispatch({ type: GET_CHARACTERS_FETCHING });
+  axios
+    .get("https://swapi.co/api/people/")
+    .then(res => {
+      dispatch({
+        type: GET_CHARACTERS_SUCCESS,
+        payload: res.data.results
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({
+        type: GET_CHARACTERS_FAILURE,
+        payload: err
+      });
+    });
+};
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,35 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import { getCharacters } from "../actions";
 import { CharacterList } from "../components";
 // import actions
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
-    // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
+      return <h2>Characters Loading...</h2>;
       // return something here to indicate that you are fetching data
     }
     return (
       <div className="CharactersList_wrapper">
+        <h1>Star Wars</h1>
         <CharacterList characters={this.props.characters} />
       </div>
     );
   }
 }
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  error: state.charsReducer.error,
+  isFetching: state.charsReducer.isFetching
+});
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
